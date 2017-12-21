1424d4a939a8bbe0cb00934942f9e8e735ce7929
querywn <querywn@outlook.com>
2017-12-21 13:10:44 +0800

    用户模块

--- src/main/java/com/neo/gyl/domain/basedata/Department.java
+++ src/main/java/com/neo/gyl/domain/basedata/Department.java
@@ -1,12 +1,15 @@
 package com.neo.gyl.domain.basedata;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Department implements Serializable {
 	
 	private Integer did;
 	private String name;
 	private String description;
+	private Set<User> users = new HashSet<User>();
 	
 	public Integer getDid() {
 		return did;
@@ -25,6 +28,12 @@ public class Department implements Serializable {
 	}
 	public void setDescription(String description) {
 		this.description = description;
+	}
+	public Set<User> getUsers() {
+		return users;
+	}
+	public void setUsers(Set<User> users) {
+		this.users = users;
 	}
 	
 	
